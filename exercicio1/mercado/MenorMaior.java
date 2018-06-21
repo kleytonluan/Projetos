9a6f46a9d3d28fcaa440080da7cd6e29d89b48e6
Luan <36345746+kleytonluan@users.noreply.github.com>
2018-06-21 13:28:54 -0300

    Update MenorMaior.java

--- exercicio1/mercado/MenorMaior.java
+++ exercicio1/mercado/MenorMaior.java
@@ -30,28 +30,3 @@ public class MenorMaior {
 		return menor;
 	}
 }
-/*	
-	public Produto maior;
-	public Produto menor;
-	public void encontrar(List<Produto> produtos) {
-		double menorValor = Double.POSITIVE_INFINITY;
-		double maiorValor = Double.NEGATIVE_INFINITY;
-		for(Produto produto : produtos) {
-			if(produto.getValor()>maiorValor) {
-				this.maior = produto;
-				maiorValor = produto.getValor();
-			}
-			if(produto.getValor()<menorValor) {
-				this.menor = produto;
-				menorValor = produto.getValor();
-			}
-		}
-	}
-	public Produto getMaior() {
-		return maior;
-	}
-	public Produto getMenor() {
-		return menor;
-	}
-}
- */
